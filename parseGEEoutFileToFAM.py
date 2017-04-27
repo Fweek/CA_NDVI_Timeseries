6c75bf4d32e74a1c11260e5e24a6a2d7f1e28357
Alberto Guzman <aguzman@csumb.edu>
2017-04-27 15:11:11 -0700

    Update parseGEEoutFileToFAM.py

--- parseGEEoutFileToFAM.py
+++ parseGEEoutFileToFAM.py
@@ -21,3 +21,24 @@ input = []
 #Read in just the columns we want to process
 for l in lines:
   input.append((l[2],l[3],l[5],l[7]))
+
+output = []
+
+#Remove header
+input = input[1::]
+
+for l in input:
+  #Get the date and change it from YYYY-MM-DD to 
+  dtStr = l[1]
+  #Ugly code but need to get this moving...
+  yr,mm,dd = dtStr[0:4],dtStr[5:7],dtStr[8::]
+  #Debug
+  print yr,mm,dd
+  dtNum = (datetime.datetime(int(yr),int(mm),int(dd)) - datetime.datetime(1980, 1, 1)).days
+  #Debug
+  print dtNum
+
+  #clean up the ndvi column, need to get rid of nulls and 'nd' and just get the numbers
+
+
+  #Push data to output list
