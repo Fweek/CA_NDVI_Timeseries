4817d81169035f742bfbbd2ca5e6c6f1b5c17c1e
Fweek <go.myco@gmail.com>
2017-05-23 12:22:11 -0700

    Update FAM_L7_timeseries_wrapper.sh

--- FAM_L7_timeseries_wrapper.sh
+++ FAM_L7_timeseries_wrapper.sh
@@ -1,19 +1,27 @@
-#!/bin/bash
-
-echo 'Testing GEE performance using 10,100,1k,5k,1k and 15k'
-
+echo 'Creating wrapper'
 mkdir outLogs
-
-time python FAM_L7_timeseries.py Test_L7_timeseries 10 2016-01-01 2016-12-31 n > ./outLogs/out_10.out 2>&1
-
-time python FAM_L7_timeseries.py Test_L7_timeseries 100 2016-01-01 2016-12-31 n > ./outLogs/out_100.out 2>&1
-
-time python FAM_L7_timeseries.py Test_L7_timeseries 1000 2016-01-01 2016-12-31 n > ./outLogs/out_1000.out 2>&1
-
-time python FAM_L7_timeseries.py Test_L7_timeseries 5000 2016-01-01 2016-12-31 n > ./outLogs/out_5000.out 2>&1
-
-time python FAM_L7_timeseries.py Test_L7_timeseries 10000 2016-01-01 2016-12-31 n > ./outLogs/out_10000.out 2>&1
-
-time python FAM_L7_timeseries.py Test_L7_timeseries 15000 2016-01-01 2016-12-31 n > ./outLogs/out_15000.out 2>&1
-
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group1 [-124.453125, 33.362, -114.0985107421875, 32.4] 2016-01-01 2016-12-31 n > ./outLogs/out_1.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group2 [-124.453125, 34.324, -114.0985107421875, 33.362] 2016-01-01 2016-12-31 n > ./outLogs/out_2.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group3 [-124.453125, 35.286, -114.0985107421875, 34.324] 2016-01-01 2016-12-31 n > ./outLogs/out_3.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group4 [-124.453125, 35.5265, -114.0985107421875, 35.286] 2016-01-01 2016-12-31 n > ./outLogs/out_4.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group5 [-124.453125, 35.766999999999996, -114.0985107421875, 35.5265] 2016-01-01 2016-12-31 n > ./outLogs/out_5.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group6 [-124.453125, 36.0075, -114.0985107421875, 35.766999999999996] 2016-01-01 2016-12-31 n > ./outLogs/out_6.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group7 [-124.453125, 36.248, -114.0985107421875, 36.0075] 2016-01-01 2016-12-31 n > ./outLogs/out_7.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group8 [-124.453125, 36.4404, -114.0985107421875, 36.248] 2016-01-01 2016-12-31 n > ./outLogs/out_8.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group9 [-124.453125, 36.632799999999996, -114.0985107421875, 36.4404] 2016-01-01 2016-12-31 n > ./outLogs/out_9.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group10 [-124.453125, 36.8252, -114.0985107421875, 36.632799999999996] 2016-01-01 2016-12-31 n > ./outLogs/out_10.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group11 [-124.453125, 37.0176, -114.0985107421875, 36.8252] 2016-01-01 2016-12-31 n > ./outLogs/out_11.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group12 [-124.453125, 37.21, -114.0985107421875, 37.0176] 2016-01-01 2016-12-31 n > ./outLogs/out_12.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group13 [-124.453125, 37.4505, -114.0985107421875, 37.21] 2016-01-01 2016-12-31 n > ./outLogs/out_13.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group14 [-124.453125, 37.691, -114.0985107421875, 37.4505] 2016-01-01 2016-12-31 n > ./outLogs/out_14.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group15 [-124.453125, 37.9315, -114.0985107421875, 37.691] 2016-01-01 2016-12-31 n > ./outLogs/out_15.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group16 [-124.453125, 38.172, -114.0985107421875, 37.9315] 2016-01-01 2016-12-31 n > ./outLogs/out_16.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group17 [-124.453125, 38.492666666666665, -114.0985107421875, 38.172] 2016-01-01 2016-12-31 n > ./outLogs/out_17.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group18 [-124.453125, 38.81333333333333, -114.0985107421875, 38.492666666666665] 2016-01-01 2016-12-31 n > ./outLogs/out_18.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group19 [-124.453125, 39.134, -114.0985107421875, 38.81333333333333] 2016-01-01 2016-12-31 n > ./outLogs/out_19.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group20 [-124.453125, 39.45466666666667, -114.0985107421875, 39.134] 2016-01-01 2016-12-31 n > ./outLogs/out_20.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group21 [-124.453125, 39.775333333333336, -114.0985107421875, 39.45466666666667] 2016-01-01 2016-12-31 n > ./outLogs/out_21.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group22 [-124.453125, 40.096000000000004, -114.0985107421875, 39.775333333333336] 2016-01-01 2016-12-31 n > ./outLogs/out_22.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group23 [-124.453125, 41.058, -114.0985107421875, 40.096000000000004] 2016-01-01 2016-12-31 n > ./outLogs/out_23.out 2>&1
+time python CA_NDVI_Timeseries.py CA_L7_timeseries_group24 [-124.453125, 42.02, -114.0985107421875, 41.058] 2016-01-01 2016-12-31 n > ./outLogs/out_24.out 2>&1
 echo 'Done'
