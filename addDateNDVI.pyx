7dce58fe02aa6ecfb477acdf875c1becae0acc2c
Fweek <go.myco@gmail.com>
2017-08-01 15:14:55 -0700

    Create addDateNDVI.pyx

---
+++ addDateNDVI.pyx
@@ -0,0 +1,33 @@
+cimport cython
+cimport numpy as np
+import numpy as np
+
+def populate (int yDim, np.ndarray[np.float_t, ndim=2] finalOutput, list tempOut):
+    cdef int row = 0
+    cdef double simsId = 0
+    cdef tuple j = ()
+    cdef int date = 0
+    cdef double ndvi = 0
+    cdef double tempOutId = 0
+    cdef int col = 0
+    cdef int dateTemp = 0
+    cdef int dateTemp2 = 0
+
+    for row in range(1, yDim):
+        simsId = finalOutput[row, 0]
+
+        for j in tempOut:
+            tempOutId = float(j[0])
+            if simsId == tempOutId:
+                print "IDs match", simsId, tempOutId
+                cdate = j[1]
+                ndvi = float(j[2])
+                print date, ndvi
+
+                for col in range(1, 48):
+                    dateTemp = int(finalOutput[0, col])
+                    dateTemp2 = dateTemp+8
+
+                    if dateTemp <= date < dateTemp2:
+                        print row, col, ndvi
+                        finalOutput[<unsigned int>row, <unsigned int>col] = ndvi
