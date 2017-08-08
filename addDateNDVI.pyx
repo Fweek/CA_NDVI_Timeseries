22ef0d7dc86a2027b398a977f6a974390b5ec3c0
Alberto Guzman <aguzman@csumb.edu>
2017-08-08 14:06:58 -0700

    Got rid some pure python elements
    Got rid of list,tuple and some other python elements that were slowing down the code.

--- addDateNDVI.pyx
+++ addDateNDVI.pyx
@@ -2,32 +2,51 @@ cimport cython
 cimport numpy as np
 import numpy as np
 
-def populate (int yDim, np.ndarray[np.float_t, ndim=2] finalOutput, list tempOut):
+def populate (int yDim, np.ndarray[np.float_t, ndim=2] finalOutput, np.ndarray[np.float_t, ndim=2] tempOut):
     cdef int row = 0
     cdef double simsId = 0
-    cdef tuple j = ()
-    cdef int date = 0
+    #cdef tuple j = ()
+    cdef int j = 0
+    cdef double date = 0
     cdef double ndvi = 0
     cdef double tempOutId = 0
     cdef int col = 0
-    cdef int dateTemp = 0
-    cdef int dateTemp2 = 0
+    cdef double dateTemp = 0
+    cdef double dateTemp2 = 0
 
-    for row in range(1, yDim):
+    cdef int jDim = tempOut.shape[0]#0
+  
+    cdef int currentIndx = 0
+
+    print yDim
+    print jDim
+
+    for row in range(1,yDim):
+        print '{0}\r'.format(row),
         simsId = finalOutput[<unsigned int>row, 0]
 
-        for j in tempOut:
-            tempOutId = float(j[0])
+        for j in range(0,jDim):#tempOut:
+            #Get the sims IDs
+            tempOutId = tempOut[<unsigned int>j,0]
+            #print tempOutId,simsId
             if simsId == tempOutId:
-                print "IDs match", simsId, tempOutId
-                cdate = j[1]
-                ndvi = float(j[2])
-                print date, ndvi
+                #currentIndx = j
+                #we could use this to start searching at last position
+                #print "IDs match", simsId, tempOutId
+                date = tempOut[<unsigned int>j,1]
+                ndvi = tempOut[<unsigned int>j,2]
+                
+                #If it's no data then move on to next row
+                if ndvi > -1.0:
+                  #print date, ndvi
 
-                for col in range(5, 51):
+                  #Find the colum
+                  for col in range(5, 51):
                     dateTemp = int(finalOutput[0, <unsigned int>col])
                     dateTemp2 = dateTemp+8
 
-                    if dateTemp <= date < dateTemp2:
-                        print row, col, ndvi
+                    if (dateTemp == date) and (date < dateTemp2):
+                        #print row, col, ndvi
                         finalOutput[<unsigned int>row, <unsigned int>col] = ndvi
+    
+    return finalOutput
