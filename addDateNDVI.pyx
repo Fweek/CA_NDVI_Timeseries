48c1596fd981dbb1af9f410aa5a71a8d60bfc63a
Fweek <go.myco@gmail.com>
2017-08-09 15:37:13 -0700

    Update addDateNDVI.pyx

--- addDateNDVI.pyx
+++ addDateNDVI.pyx
@@ -15,42 +15,42 @@ def populate (int yDim, np.ndarray[np.float_t, ndim=2] finalOutput, np.ndarray[n
     cdef double dateTemp2 = 0
 
     cdef int jDim = tempOut.shape[0]#0
-  
+
     cdef int currentIndx = 0
 
     print yDim
     print jDim
 
-    for row in range(1,yDim):
+    for row in range(1,yDim): # for each row in the range of SIMs IDs (~300000)
         print '{0}\r'.format(row),
-        simsId = finalOutput[<unsigned int>row, 0]
+        simsId = finalOutput[<unsigned int>row, 0] # create variable called simsId, set it equal to the simsId in the [row, 0] position
 
-        for j in range(0,jDim):#tempOut:
+        for j in range(0,jDim):#tempOut: # then in a different list
             #Get the sims IDs
-            tempOutId = tempOut[<unsigned int>j,0]
+            tempOutId = tempOut[<unsigned int>j,0] # make tempOutId equal to just the SIMS ID column
+            # Check the date and add to column
             #print tempOutId,simsId
             if simsId > tempOutId:
                 break
-                
-            if simsId == tempOutId:
+
+            if simsId == tempOutId: # check to see if the two SIMS ID match up
                 #currentIndx = j
                 #we could use this to start searching at last position
-                #print "IDs match", simsId, tempOutId
-                date = tempOut[<unsigned int>j,1]
+                #print "IDs match", simsId, tempOutId # if they match says so
+                date = tempOut[<unsigned int>j,1]  # create variables for each column
                 ndvi = tempOut[<unsigned int>j,2]
-                
+
                 #If it's no data then move on to next row
                 if ndvi > -1.0:
                   #print date, ndvi
 
                   #Find the colum
                   for col in range(5, 51):
-                    dateTemp = int(finalOutput[0, <unsigned int>col])
+                    dateTemp = int(finalOutput[0, <unsigned int>col])   # make new date variable based on header
                     dateTemp2 = dateTemp+8
 
                     if (dateTemp == date) and (date < dateTemp2):
                         #print row, col, ndvi
                         finalOutput[<unsigned int>row, <unsigned int>col] = ndvi
-                
-    
+
     return finalOutput
