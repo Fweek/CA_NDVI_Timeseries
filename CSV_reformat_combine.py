2d143c65bd298d0b15fbae13390af5c67441841f
Fweek <go.myco@gmail.com>
2018-01-22 15:30:44 -0800

    Update CSV_reformat_combine.py

--- CSV_reformat_combine.py
+++ CSV_reformat_combine.py
@@ -22,99 +22,54 @@ bTime = datetime.datetime.now()
 #Set working directory to user input (directory path of input files)
 os.chdir(sys.argv[1])
 
-#Make a new directory for the combined output files if it does not already exist
-if not os.path.exists('Output_Combined'):
-    os.makedirs('Output_Combined', )
-
-#Count how many files are in the directory. Half of them should be L7 and the other half L8
-filecount = len([f for f in os.listdir(sys.argv[1]) if os.path.isfile(f)])
-
-#Divide the count in half
-filecount_half = int(filecount/2)
-
-#Take the first filename and parse it out
-files = os.listdir(sys.argv[1]) #create list of all the filenames in the directory
-filename = str(files[1]) #create string object of the first filename
-filename_split = filename.split('_') #split the filename string up by _
-split_length = len(filename_split) #count the number of splits
-
-sat_index = int(split_length-2) #the split that identifies the satellite used for that file is always the second to last
-sat_string = filename_split[sat_index] #create string object of the satellite sensor (L7SR, L7TOA, L8SR, L8TOA)
-sat_string_split = list(sat_string) #split the string into a list of characters
-sat_string_split_count = len(sat_string_split) #count the number of characters in the list
-sat_type = sat_string_split[2:sat_string_split_count] #isolate the characters for the satellite type
-sat_type_join =  ''.join(sat_type) #rejoin the characters for satellite type (SR, TOA)
-
-offset_index = int(split_length-1) #the split that identifies the offset used for that file is always the last
-offset_name = filename_split[offset_index] #this split still has .csv at the end so we need to split it again to get rid of .csv
-offset_name_split = offset_name.split('.') #split by .
-offset_value = int(offset_name_split[0])
-
-#Rejoin the original filename
-prefix = filename_split[0:sat_index]
-prefix_join = '_'.join(prefix)
-
-#Merge L7 and L8 files together while overwriting L7 values with L8 values if the L7 value is 0
-for i in range(filecount_half-1): #loop for each pair of landsat files
-    L7csv = numpy.genfromtxt(str(prefix_join)+'_L7'+str(sat_type_join)+'_'+str(offset_value)+".csv", delimiter= ",")
-    L8csv = numpy.genfromtxt(str(prefix_join)+'_L8'+str(sat_type_join)+'_'+str(offset_value)+".csv", delimiter= ",")
-    output = numpy.where(L8csv > 0, L8csv, L7csv)
-    output_destination = str(sys.argv[1])+'/Output_Combined'
-    numpy.savetxt(output_destination, output, delimiter=",", fmt='%.3f')
-    offset_value = offset_value+15000 #python WA_NDVI_Timeseries_T2.py L7SR WA_meanNDVI_timeseries 2013-01-01 2013-12-31 n
-
-
-#Set working directory to user input (directory path of input files)
-os.chdir(str(sys.argv[1])+'/Output_Combined')
-
-# Make a new directory for the output files if it does not already exist
+#Make a new directory for the output files if it does not already exist
 if not os.path.exists('Output'):
     os.makedirs('Output', )
 
-# Loop through every file in the current working directory.
+#Loop through every file in the current working directory.
 for csvFilename in os.listdir('.'):
     if not csvFilename.endswith('.csv'):
         continue  # skip non-csv files
 
     print('Modifying ' + csvFilename + '...')
 
-    # Open the file
+    #Open the file
     fPtr = open(csvFilename)
     if fPtr is None:  # if the file doesn't exist close out ???
         print "Error opening %s " % csvFilename
         sys.exit(1)
 
-    # Read in the file using csv module
+    #Read in the file using csv module
     rdr = csv.reader(fPtr)
     fPtr = None
 
-    # Create a blank list container for input
+    #Create a blank list container for input
     input = []
 
-    # Keep only columns we want: DATE in column 1, NDVI in column 2, and SIMSID in column 3
+    #Keep only columns we want: DATE in column 1, NDVI in column 2, and SIMSID in column 3
     for l in rdr:  # for each line in the current open CSV
         input.append((l[1], l[2], l[3]))  # take columns 1-3 and add them to the empty list container
 
-    # Remove header
-    input = input[1::]  # keep all rows but the header
+    #Remove header
+    input = input[1::]  #keep all rows but the header
 
-    # Find how many unique ids we have
+    #Find how many unique ids we have
     simsIds = [float(i[2]) for i in input] # create subset list that is just the 2nd column (SIMs ID column) of the new list
 
-    # Get all the unique simsids and count them. This will be the number of rows for our final table
+    #Get all the unique simsids and count them. This will be the number of rows for our final table
     uniqueIds = numpy.unique(simsIds)  # count all the unique SIMS IDs in the subset list
     yDim = len(uniqueIds) + 1  # Make a new variable that is = to the # of unique IDs; adding 1 to account for new row header
     print "Number of unique sims ids %d" % yDim
 
-    # We will always have 51 columns. 5 extra + 46 timesteps
+    #We will always have 51 columns. 5 extra + 46 timesteps
     xDim = 51
 
-    # Create final output array
-    finalOutput = numpy.ones((yDim, xDim)) * -9999.0  # fill all cells with -9999
+    #Create final output array
+    finalOutput = numpy.ones((yDim, xDim)) * -9999.0  #fill all cells with -9999
 
-    # Create temp output container
+    #Create temp output container
     tempOut = numpy.ones(
-        (len(input), 3)) * -9999.0  # create a temp output array with only 3 columns and fill all cells with -9999
+        (len(input), 3)) * -9999.0  #create a temp output array with only 3 columns and fill all cells with -9999
 
     print "Reformatting the array"
 
@@ -126,7 +81,7 @@ for csvFilename in os.listdir('.'):
         except ValueError:
             return False
 
-    # More formatting
+    #More formatting
     for i in range(0, len(input)):  # for each row in the range of 0 to however long input is
         inputItem = input[i]  # create list with just the current row. Ex. [01/05/2016, 1536, 0.56]
         dtStr = inputItem[0]  # create new list of just 0 cell(date cell)
@@ -219,5 +174,53 @@ for csvFilename in os.listdir('.'):
     output_destination = sys.argv[1] + '/Output/' + 'Reformatted' + csvFilename
     numpy.savetxt(output_destination, finalOutput, delimiter=",", fmt='%.3f')
 
+print "Finished reformatting files."
 print "Start time: ", bTime
 print "End time: ", datetime.datetime.now()
+
+
+print "Combining files..."
+
+#Set working directory to Output directory of reformated files
+os.chdir(str(sys.argv[1])+'/Output')
+
+#Make a new directory for the combined output files if it does not already exist
+if not os.path.exists('Output_Combined'):
+    os.makedirs('Output_Combined', )
+
+#Count how many files are in the directory. Half of them should be L7 and the other half L8
+filecount = len([f for f in os.listdir(str(sys.argv[1])+'/Output') if os.path.isfile(f)])
+
+#Divide the count in half
+filecount_half = int(filecount/2)
+
+#Take the first filename and parse it out
+files = os.listdir(sys.argv[1]) #create list of all the filenames in the directory
+filename = str(files[1]) #create string object of the first filename
+filename_split = filename.split('_') #split the filename string up by _
+split_length = len(filename_split) #count the number of splits
+
+sat_index = int(split_length-2) #the split that identifies the satellite used for that file is always the second to last
+sat_string = filename_split[sat_index] #create string object of the satellite sensor (L7SR, L7TOA, L8SR, L8TOA)
+sat_string_split = list(sat_string) #split the string into a list of characters
+sat_string_split_count = len(sat_string_split) #count the number of characters in the list
+sat_type = sat_string_split[2:sat_string_split_count] #isolate the characters for the satellite type
+sat_type_join =  ''.join(sat_type) #rejoin the characters for satellite type (SR, TOA)
+
+offset_index = int(split_length-1) #the split that identifies the offset used for that file is always the last
+offset_name = filename_split[offset_index] #this split still has .csv at the end so we need to split it again to get rid of .csv
+offset_name_split = offset_name.split('.') #split by .
+offset_value = int(offset_name_split[0])
+
+#Rejoin the original filename
+prefix = filename_split[0:sat_index]
+prefix_join = '_'.join(prefix)
+
+#Merge L7 and L8 files together while overwriting L7 values with L8 values if the L7 value is 0
+for i in range(filecount_half-1): #loop for each pair of landsat files
+    L7csv = numpy.genfromtxt(str(prefix_join)+'_L7'+str(sat_type_join)+'_'+str(offset_value)+".csv", delimiter= ",")
+    L8csv = numpy.genfromtxt(str(prefix_join)+'_L8'+str(sat_type_join)+'_'+str(offset_value)+".csv", delimiter= ",")
+    output = numpy.where(L8csv > 0, L8csv, L7csv)
+    output_destination = str(sys.argv[1])+'/Output_Combined'
+    numpy.savetxt(output_destination, output, delimiter=",", fmt='%.3f')
+    offset_value = offset_value+15000 #python WA_NDVI_Timeseries_T2.py L7SR WA_meanNDVI_timeseries 2013-01-01 2013-12-31 n
