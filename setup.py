54b7bb739ea3507fe1e2dc8a7b436f56e995330f
Fweek <go.myco@gmail.com>
2018-02-05 15:00:34 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 # python setup.py build_ext --inplace
 #If you get "ImportError: No module named Cython.Distutils" then check if you have cython installed.
 # pip install cython
+#For more help: https://github.com/cython/cython/wiki/InstallingOnWindows
+#https://github.com/cython/cython/wiki/CythonExtensionsOnWindows
 
 from setuptools import setup
 from setuptools import Extension
