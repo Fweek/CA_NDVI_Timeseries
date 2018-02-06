97b092da42a5a59f993d5c0b8954feca60e0c46b
Fweek <go.myco@gmail.com>
2018-02-05 16:16:28 -0800

    Update FAM_Wrapper.sh

--- FAM_Wrapper.sh
+++ FAM_Wrapper.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env/python
+#This is a wrapper bash script that runs the following four Python scripts in sequential order. 
+#This wrapper script requires two parameters to run: the directory location of the RAW input files and the year of the RAW input files.
 
-input1=$1
-input2=$2
+input1=$1 #Parameter 1: directory location of the RAW input files Example: C:\Users\Michael\Desktop\FAM
+input2=$2 #Parameter 2: year of the RAW input files. Example: 2013
 
 python CSV_reformat.py $input1 $input2
 python CSV_combine.py $input1
